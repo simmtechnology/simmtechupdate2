6baeb5b1c4814d6ee8dd304a7e39460ab990eeb9
simmtechnology <simmtechnology@gmail.com>
2020-08-23 20:35:50 +0200

    First Commit

---
+++ src/test/java/zw/co/clouddynamics/SimmtechApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.clouddynamics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimmtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
